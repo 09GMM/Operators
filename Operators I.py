7894e277de3a15397da5128cecc92fda48c9ee14
09GMM <guneshmakhija@gmail.com>
2025-10-28 18:50:51 -0500

    Add files via upload

---
+++ Operators I.py
@@ -0,0 +1,15 @@
+#There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+
+tree_1 = 98
+tree_2 = 94
+tree_3 = 41
+tree_4 = 95
+tree_5 = 11
+
+#finding totl.
+sum = tree_1+tree_2+tree_3+tree_4+tree_5
+print ("The sum of all trees is: ", sum)
+
+#finding avrg.
+average = sum/5
+print ("the average measurment of a tree is: ", average)
